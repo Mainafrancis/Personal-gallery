e6cb8dbb61633aad3b222d221e9ee30fc4fbd2bb
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-07 21:29:46 +0300

    image viewer

--- pictures/views.py
+++ pictures/views.py
@@ -5,3 +5,8 @@ from .models import Image,Location,Category
 #Create your views here.
 def image(request):
     image = Image.get_all()
+    return render(request,"all_images/photo.html", {"image":image})
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET .get("image")    

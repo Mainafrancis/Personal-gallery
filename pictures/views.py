5d42c31f145772baa6f6235d4b62170f4aee0b07
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-07 21:34:16 +0300

    else statement

--- pictures/views.py
+++ pictures/views.py
@@ -9,4 +9,11 @@ def image(request):
 def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET .get("image")    
+        search_term = request.GET .get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all_images/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"         

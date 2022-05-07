96272d62071390cee9b1e9a5e656ee8085159f7c
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-07 21:23:18 +0300

    url verification

--- pictures/urls.py
+++ pictures/urls.py
@@ -3,3 +3,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin', admin.site.urls),
+    url('^$',views.image,name = 'image'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^detail/(\d+)', views.details , name='detail'),
+    url(r'^location/(\d+)', views.location, name='location'),
+
+
+]
+## new static route that references the location to the upload files
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
